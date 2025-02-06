cd140bd69a2235c48a832ee7db5925338981c605
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2025-02-06 23:31:05 +0530

    cors changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config({
 });
 
 app.use(cors({
-  origin:['http://localhost:5174','http://localhost:5173'],
+  origin:['http://localhost:5174','http://localhost:5173','https://filer-frontend.vercel.app'],
   credentials:true,
   methods:['GET','POST','DELETE','PUT','PATCH']
 }));
